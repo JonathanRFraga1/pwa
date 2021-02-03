44dbeaf6619db7ac6697ec7f94e20f9aac75d6b5
JonathanRFraga1 <jonathanrossettodefraga@gmail.com>
2021-02-03 09:31:54 -0300

    Adicionado o tema one ui

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,30 @@
 const cacheName = 'cache-2';
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll(
-          [
-            './offline.html',
-            './_includes/css/styles.css',
-            './_includes/fontAwesome/css/all.css',
-            './_manifest/icon-512x512.png'
-          ]
-        );
-      })
-    );
-  });
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(
+        [
+          './offline.html',
+          './_includes/css/styles.css',
+          './_includes/fontAwesome/css/all.css',
+          './_manifest/icon-512x512.png'
+        ]
+      );
+    })
+  );
+});
 
 this.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return caches.match('./offline.html');
-            })
-    )
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match(
+            './offline.html'
+        );
+      })
+  )
 });
