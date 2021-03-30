0542de291cebf66fde7572bf16b6e7c397a65ec3
JonathanRFraga1 <jonathanrossettodefraga@gmail.com>
2021-03-30 01:06:57 -0300

    Adicionado roteamento basico

--- _includes/js/scripts.js
+++ _includes/js/scripts.js
@@ -1,3 +1,36 @@
+let hash = location.hash;
+let page = '';
+
+if (hash == '') {
+  page = 'home';
+  location.hash = '#home'
+} else {
+  page = hash.substring(hash.indexOf('#') + 1, hash.length);
+}
+
+carregaPagina(page);
+
+window.onhashchange = () => {
+  let hash = location.hash;
+  let page = '';
+
+  if (hash == '') {
+    page = 'home';
+    location.hash = '#home'
+  } else {
+    page = hash.substring(hash.indexOf('#') + 1, hash.length);
+  }
+  
+  carregaPagina(page);
+}
+
+$('a').click((event) => {
+  event.preventDefault();
+  let hash = event.target.href
+  hash = hash.substring(hash.indexOf('#') + 1, hash.length);
+  location.hash = hash;
+});
+
 function registraServiceWorker() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js')
@@ -8,4 +41,37 @@ function registraServiceWorker() {
         console.log('Service worker registration failed, error:', error);
       });
   }
+}
+
+function carregaPagina(pagina) {
+  $.ajax({
+    url: `/pages/${pagina}.html`,
+    success: function (data) {
+      $("#content").html(data);
+    },
+    error: function (data) {
+      console.error(data);
+      carregaPaginaErro();
+    }
+  });
+}
+
+function carregaPaginaErro() {
+  $.ajax({
+    url: `/pages/404.html`,
+    success: function (data) {
+      $("#content").html(data);
+    },
+    error: function (data) {
+      console.error(data);
+    }
+  });
+}
+
+function isLogged() {
+  let user = localStorage.getItem("user");
+  if (user != null) {
+    return true;
+  }
+  return false;
 }
